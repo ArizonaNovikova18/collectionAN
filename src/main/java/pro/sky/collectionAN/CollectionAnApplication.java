029254f4171c1f6ba2ecad999ca226423c8c4f65
User <xtoycohcohu@mail.ru>
2024-05-06 19:56:57 +0700

    master

---
+++ src/main/java/pro/sky/collectionAN/CollectionAnApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.collectionAN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionAnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionAnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/collectionAN/CollectionAnApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.collectionAN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionAnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
